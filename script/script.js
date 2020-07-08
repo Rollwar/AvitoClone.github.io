dfbd3d2c8c91f37a9101e81d395933c42815e254
Rollwar <artem.ratkevich.95@mail.ru>
2020-07-08 19:22:22 +0300

    add listerners for modalItem

--- script/script.js
+++ script/script.js
@@ -3,7 +3,9 @@
 const modalAdd = document.querySelector(".modal__add"),
     addAd = document.querySelector(".add__ad"),
     modalBtnSubmit = document.querySelector('.modal__btn-submit'),
-    modalSubmit = document.querySelector('.modal__submit');
+    modalSubmit = document.querySelector('.modal__submit'),
+    modalCatalog = document.querySelector('.catalog'),
+    modalItem = document.querySelector('.modal__item');
 
 addAd.addEventListener("click", () => {
     modalAdd.classList.remove('hide');
@@ -20,8 +22,29 @@ modalAdd.addEventListener("click", event => {
     }
 });
 
+modalItem.addEventListener("click", event =>{
+    const target = event.target;
+
+    if(target.classList.contains('modal__close') ||
+    target === modalItem){
+        modalItem.classList.add('hide');
+    }
+})
+
+modalCatalog.addEventListener("click", event =>{
+
+    const target = event.target;
+
+    if(target.closest('.card')){
+        modalItem.classList.remove('hide');
+   }
+
+});
+
 document.addEventListener("keydown", (event) => {
     if(event.which == "27"){
         modalAdd.classList.add('hide');
+        modalItem.classList.add('hide');
     }
 });
+
